4a3eee566e841c9fbf17f40c8e82a4ec709b8f06
ncperes <nperes@psgtecnologia.com.br>
2022-11-17 13:22:46 -0400

    melhorias na tela de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,70 +1,63 @@
-export default function Login() {
+const Login = () => {
   return (
-    <div className="md:container mt-40 mb-40 h-screen">
-      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-6 justify-center rounded-md h-2/3 ml-10 w-4/5">
-        <div className="bg-logo-insted bg-contain bg-no-repeat h-24 w-full m-3.5"></div>
-        <div className="h-10 mt-8 p-14 w-full justify-center flex items-center text-2xl text-white border-t">
-          <h1>Login - Área Insted</h1>
+    <div className="md:container fixed mt-40 mb-40 h-screen">
+      <div className="bg-verde-insted grid gap-0 grid-cols-1 grid-rows-6 justify-center rounded-md h-3/5 ml-12 w-80">
+        <div className="bg-logo-insted bg-contain bg-no-repeat h-16 w-full mt-5 ml-14"></div>
+        <div className="h-10 pt-10 p-14 mt-4 w-full justify-center flex items-center text-3xl text-white border-t">
+          <h1>LOGIN</h1>
         </div>
-        <div className="grid gap-0 grid-cols-1 grid-rows-2 mt-5 w-full justify-center">
-          <div className="mt-5 ml-6 w-full justify-left">
-            <label className="text-white">Usuário:</label>
-          </div>
-          <div className="w-full justify-center flex mt-2">
-            <input
-              type="text"
-              placeholder=" Informe seu RA"
-              className="flex
+        <div className="w-full justify-center flex mt-7">
+          <input
+            type="text"
+            placeholder=" Informe seu RA"
+            className="flex
               justify-center
               items-center
-              w-72
+              w-64
               h-8
-              rounded-md
-              bg-slate-200
-              hover:bg-white
-              placeholder:text-slate-400
-              border-slate-400
+              text-white
+              bg-verde-insted
+              placeholder:text-white
+              border-white
+              focus:outline-none
               border-solid
-              border"
-            />
-          </div>
+              border-b"
+          />
         </div>
-        <div className="grid gap-0 grid-cols-1 grid-rows-2 w-full justify-center">
-          <div className="mt-6 ml-6 w-full justify-left">
-            <label className="text-white">Senha:</label>
-          </div>
-          <div className="w-full justify-center flex">
-            <input
-              type="text"
-              placeholder=" Informe sua senha"
-              className="flex
+        <div className="w-full justify-center flex mt-2">
+          <input
+            type="password"
+            placeholder=" Informe sua senha"
+            className="flex
               justify-center
               items-center
-              w-72
+              w-64
               h-8
-              rounded-md
-              bg-slate-200
-              hover:bg-white
-              placeholder:text-slate-400
-              border-slate-400
+              text-white
+              bg-verde-insted
+              placeholder:text-white
+              border-white
+              focus:outline-none
+              focus:
               border-solid
-              border"
-            />
-          </div>
+              border-b"
+          />
         </div>
-        <div className="grid gap-0 grid-cols-1 grid-rows-2 mt-3">
-          <div className="w-full flex justify-center h-8">
-            <button className="bg-black text-white text-xl h-10 w-36 rounded">ENTRAR</button>
-          </div>
-          <div className="flex justify-center w-full mt-2.5">
-            <input type="checkbox" className="mb-4 mr-1.5" />
-            <label className="text-sm mb-2 text-slate-700">Continuar conectado</label>
-          </div>
+        <div className="w-full flex justify-center mt-2 h-8">
+          <button className="bg-white shadow-md text-verde-insted text-xl h-12 w-48 rounded-3xl">
+            ENTRAR
+          </button>
         </div>
-        <div className="w-full flex justify-center mt-16">
-          <h4 className="text-slate-700 hover:underline text-sm">Esqueci minha senha</h4>
+        <div className="flex justify-center w-full mt-10">
+          <input type="checkbox" className="mr-1 mb-8" />
+          <label className="text-xs text-slate-700 mt-0.5">Continuar conectado?</label>
+          <h4 className="ml-5 mt-0.5 text-slate-700 hover:underline text-xs">
+            Esqueci minha senha
+          </h4>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Login;
